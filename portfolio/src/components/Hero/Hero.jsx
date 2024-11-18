42058629f0bc4c31a1c9b025ff0055dd0f337313
Trish1999 <trishnarani28@gmail.com>
2024-11-19 00:03:46 +0530

    made changes in banner

--- portfolio/src/components/Hero/Hero.jsx
+++ portfolio/src/components/Hero/Hero.jsx
@@ -1,30 +1,34 @@
 import React from "react";
-
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
   return (
     <section className={styles.container}>
+      <div className={styles.background}>
       <div className={styles.content}>
-         <h1 className={styles.title}>Hi, I'm  
-        <h1 id={styles.flip}>
-          <span className={styles.flipItem}> Trishna</span>
-          <span className={styles.flipItem}> a Fullstack Developer</span>
-        </h1>
-        </h1>
-        <p className={styles.description}>
-        Experienced Software Engineer with 1.5 years in Full Stack Development,
+         <h1 className={styles.title}>Hi, I'm 
+            <span className={styles.highlight}> a Full Stack Web Developer</span>
+          </h1>
+       <p className={styles.description}>
+             Experienced Software Engineer with 1.5 years in Full Stack Development,
         specializing in UI/UX design, API integration, and database management.</p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
-          Contact Me
-        </a>
-      </div>
-      <img
-        src={getImageUrl("hero/hero.jpg")}
-        alt="Hero image of me"
-        className={styles.heroImg}
-      />
+          <a href="#resume" className={styles.resumeBtn}>
+            My Resume
+          </a>
+        </div>
+        <div className={styles.profile}>
+          <img
+            src={getImageUrl("hero/hero1.png")} 
+            alt="profile picture"
+            className={styles.profileImage}
+          />
+          <p className={styles.profileName}>
+            Trishnarani Patel <br />
+          </p>
+        </div>
+      </div>      
     </section>
+    
   );
 };
